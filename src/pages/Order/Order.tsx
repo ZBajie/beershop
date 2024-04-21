7d98d319af0327e2c1195d30ec6b5b59185883a0
Lars Abelsson <georg483@msn.com>
2024-04-21 15:37:28 +0200

    Order page

---
+++ src/pages/Order/Order.tsx
@@ -0,0 +1,12 @@
+import Cart from "../../components/Cart/Cart"
+import "./Order.scss"
+const Order = () => {
+  return (
+    <section className="order">
+      <h2>Order</h2>
+      <Cart />
+    </section>
+  )
+}
+
+export default Order
