f706fe93c9c177bc8de65806174b23ae2871a00c
Lars Abelsson <georg483@msn.com>
2024-04-21 15:37:02 +0200

    beers list

---
+++ src/components/Beers/Beers.tsx
@@ -0,0 +1,48 @@
+import "./Beers.scss"
+import { useDispatch, useSelector } from "react-redux"
+import { AppDispatch, RootState } from "../../state/store"
+import { useEffect } from "react"
+import { addToCart } from "../../state/cartSlice/cartSlice"
+
+const Beers = () => {
+  const beers = useSelector((state: RootState) => state.beers)
+  const cart = useSelector((state: RootState) => state.cart)
+  const dispatch = useDispatch<AppDispatch>()
+  useEffect(() => {
+    console.log(cart)
+  }, [cart])
+
+  return (
+    <section className="beers">
+      {beers.beers.map((beer) => (
+        <article key={beer.id}>
+          <h3>{beer.name}</h3>
+          <div>
+            <div>
+              <p>{beer.alcohol} % vol.</p>
+              <p>{beer.volume} ml</p>
+              <p>{beer.price} skr</p>
+              <button
+                onClick={() =>
+                  dispatch(
+                    addToCart({
+                      id: beer.id,
+                      name: beer.name,
+                      price: beer.price,
+                      quantity: 1,
+                    })
+                  )
+                }
+              >
+                Add to cart
+              </button>
+            </div>
+            <img src={beer.image} alt={beer.name} />
+          </div>
+        </article>
+      ))}
+    </section>
+  )
+}
+
+export default Beers
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,18 @@
+import { useDispatch, useSelector } from "react-redux"
+import Beers from "../../components/Beers/Beers"
+import { AppDispatch, RootState } from "../../state/store"
+
 const Home = () => {
-  return <div>Home</div>
+  const cart = useSelector((state: RootState) => state.cart)
+  const dispatch = useDispatch<AppDispatch>()
+
+  return (
+    <section>
+      <h1>Home</h1>
+      <Beers />
+      {cart && <p>Total: {cart.cart.length}</p>}
+    </section>
+  )
 }
 
 export default Home
