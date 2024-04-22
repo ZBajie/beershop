5e6999344789c22b86fe70e508cd49c8fe5bd656
Lars Abelsson <georg483@msn.com>
2024-04-22 16:07:37 +0200

    header image

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,15 @@
 import "./Header.scss"
 import { Link } from "react-router-dom"
 import cartSvg from "../../assets/shopping-cart-17-svgrepo-com.svg"
-
+import carlsberg from "../../assets/beers/carlsberg.png"
+import leffe from "../../assets/beers/leffe.png"
 const Header = () => {
   return (
     <header className="header-main">
-      <Link to="/">🍺</Link>
+      <Link to="/">
+        <img src={carlsberg} className="beer" alt="Beer" />
+        <img src={leffe} alt="Beer" className="beer" />
+      </Link>
       <div>
         <h1>Beers for tears</h1>
         <p>Buy your favorite beer</p>
