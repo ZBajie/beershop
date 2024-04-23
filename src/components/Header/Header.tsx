bce70cf6010040fd5206c45c4d39560979a6983c
Lars Abelsson <georg483@msn.com>
2024-04-23 12:51:27 +0200

    Changed shopping cart images

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,14 @@
 import "./Header.scss"
 import { Link } from "react-router-dom"
-import cartSvg from "../../assets/shopping-cart-17-svgrepo-com.svg"
+import cartSvg from "../../assets/cart-large-minimalistic-svgrepo-com.svg"
+import cartSvgChecked from "../../assets/cart-check-svgrepo-com.svg"
 import carlsberg from "../../assets/beers/carlsberg.png"
 import leffe from "../../assets/beers/leffe.png"
+import { useSelector } from "react-redux"
+import { RootState } from "../../state/store"
 const Header = () => {
+  const cart = useSelector((state: RootState) => state.cart)
+
   return (
     <header className="header-main">
       <Link to="/">
@@ -15,7 +20,11 @@ const Header = () => {
         <p>Buy your favorite beer</p>
       </div>
       <Link to="/order">
-        <img className="cart-svg" src={cartSvg} alt="cart" />
+        {cart.cart.length > 0 ? (
+          <img className="cart-svg" src={cartSvgChecked} alt="cart" />
+        ) : (
+          <img className="cart-svg" src={cartSvg} alt="cart" />
+        )}
       </Link>
     </header>
   )
